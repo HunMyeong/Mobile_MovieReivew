971df812d3990c6eea4db6253631d67eb4f71502
HunMyeong <102712296+HunMyeong@users.noreply.github.com>
2023-06-10 21:55:21 +0900

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,184 @@
+package com.example.apitest2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    //영화진흥위원회 통합전산망에서 발급받은 인증키
+    String apiKey="3e74b60fe100af75533107c1f2f0f55b";
+
+    ListView listView;
+    ArrayAdapter adapter;
+
+    ArrayList<String> items=new ArrayList<String>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("DIT 영화관");
+
+        listView=findViewById(R.id.listview);
+        adapter= new ArrayAdapter(this,android.R.layout.simple_list_item_1,items);
+        //원래 layout을 .xml을 만들어야 하지만 예제이므로 안드로이에서 제공하는 것(android.R.layout.simple_list_item_1)을 사용
+        listView.setAdapter(adapter);
+    }
+
+    public void clickBtn(View view) {
+        //네트워크를 통해서 xml문서를 읽어오기..
+        new Thread(){
+            @Override
+            public void run() {
+                //영화진흥위원회 open API를 통해
+                //일일박스오피스 정보를 가진 xml문서를
+                //읽어와서 분석하여 Listview에 보여주기
+                items.clear();
+
+                Date date=new Date();//현재 날짜와 시간을 가진 객체
+                date.setTime(date.getTime()-(1000*60*60*24)); //현재 날짜의 1일을 뺀 날짜
+
+                SimpleDateFormat sdf= new SimpleDateFormat("yyyyMMdd"); //SimpleDateFormat 자동으로 편리하게 포맷을 넣을 수 있다.
+                String dateStr=sdf.format(date);
+
+                String adress = " http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.xml"
+                        +"?key="+apiKey
+                        +"&targetDt="+dateStr
+                        +"&itemPerPage=10";
+                //주소 뒤에 [? key=Value & key = value id= aaa & pw= 1234] 이게 GET방식
+
+                //adress="http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.xml?key=430156241533f1d058c603178cc3ca0e&targetDt=20190919";
+                //위 xml문서의 주소(address)에 스트림을 연결하여 데이터를 읽어오기
+
+
+                try {
+                    //URL객체생성
+                    URL url= new URL(adress);
+
+                    //Stream 열기                                     //is는 바이트 스트림이라 문자열로 받기위해 isr이 필요하고 isr을 pullparser에게 줘야하는데
+                    InputStream is= url.openStream(); //바이트스트림
+                    //문자스트림으로 변환
+                    InputStreamReader isr=new InputStreamReader(is);
+
+                    //읽어들인 XML문서를 분석(parse)해주는 객체 생성    //pullparser를 만들려면 Factory가 필요해서 팩토리 만들고 pullparser를 만들었다. 결론, 그리고 pullparser에게 isr연결
+                    XmlPullParserFactory factory= XmlPullParserFactory.newInstance();
+                    XmlPullParser xpp=factory.newPullParser();
+                    xpp.setInput(isr);
+
+                    //xpp를 이용해서 xml문서를 분석
+
+                    //xpp.next();   //XmlPullParser는 시작부터 문서의 시작점에 있으므로 next해주면 START_DOCUMENT를 못만난다.
+                    int eventType= xpp.getEventType();
+
+                    String tagName;
+                    StringBuffer buffer=null;
+
+                    while(eventType!=XmlPullParser.END_DOCUMENT){
+
+                        switch (eventType){
+                            case XmlPullParser.START_DOCUMENT:
+
+                                runOnUiThread(new Runnable() {  //여기는 별도 스레드이므로 화면 구성을 하려면 runOnUiThread 필요
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(MainActivity.this,"목록 조회중.",Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+
+                                break;
+
+                            case XmlPullParser.START_TAG:
+                                tagName=xpp.getName();
+                                if(tagName.equals("dailyBoxOffice")){
+                                    buffer=new StringBuffer();
+
+                                }else if(tagName.equals("rank")){
+                                    buffer.append("순위:");
+                                    xpp.next();
+                                    buffer.append(xpp.getText()+"\n");  //아래 두줄을 한줄로 줄일 수 있다.
+//                                    String text = xpp.getText();
+//                                    buffer.append(text+"\n");
+
+                                }else if(tagName.equals("movieNm")){
+                                    buffer.append("제목:");
+                                    xpp.next();
+                                    buffer.append(xpp.getText()+"\n");
+
+                                }else if(tagName.equals("openDt")){
+                                    buffer.append("개봉일:");
+                                    xpp.next();
+                                    buffer.append(xpp.getText()+"\n");
+
+                                }else if(tagName.equals("audiAcc")){
+                                    buffer.append("누적관객수:");
+                                    xpp.next();
+                                    buffer.append(xpp.getText()+"\n");
+                                }
+                                break;
+
+                            case XmlPullParser.TEXT:
+                                break;
+
+                            case XmlPullParser.END_TAG:
+                                tagName = xpp.getName();
+                                if(tagName.equals("dailyBoxOffice")){
+
+                                    items.add(buffer.toString());
+
+                                    //리스트뷰 갱신
+                                    runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            adapter.notifyDataSetChanged();
+                                        }
+                                    });
+                                }
+                                break;
+                        }
+
+                        eventType=xpp.next();
+                        //                        xpp.next();   //두줄을 한줄로 쓸 수 있다.
+                        //                        eventType=xpp.getEventType();
+                    }//while ..
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(MainActivity.this, "조회 완료!!",Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+
+                } catch (MalformedURLException e) { e.printStackTrace();} catch (IOException e) {e.printStackTrace();} catch (XmlPullParserException e) {e.printStackTrace();}
+
+
+            }// run() ..
+        }.start();
+
+        // 버튼 클릭 시 ReviewActivity로 전환
+        if (view.getId() == R.id.btn_review) {
+            Intent intent = new Intent(MainActivity.this, ReviewActivity.class);
+            startActivity(intent);
+        }
+    }
+
+
+}
---
+++ ReviewActivity.java
@@ -0,0 +1,187 @@
+package com.example.apitest2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class ReviewActivity extends AppCompatActivity {
+    private ReviewDatabaseHelper dbHelper;
+    private SQLiteDatabase database;
+    String apiKey = "3e74b60fe100af75533107c1f2f0f55b";
+    private EditText editTextName;
+    private Spinner spinnerMovie;
+    private EditText editTextReview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_review);
+        setTitle("한줄평");
+
+        editTextName = findViewById(R.id.editTextName);
+        spinnerMovie = findViewById(R.id.spinnerMovie);
+        editTextReview = findViewById(R.id.editTextReview);
+
+        dbHelper = new ReviewDatabaseHelper(this);
+        database = dbHelper.getWritableDatabase();
+
+        // AsyncTask를 사용하여 네트워크 작업 수행
+        new FetchMovieTitlesTask().execute();
+
+        Button buttonSave = findViewById(R.id.buttonDone);
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveReview();
+                updateReviewList(); // 리뷰 저장 후 목록 업데이트
+            }
+        });
+
+        updateReviewList(); // 액티비티 시작 시 리뷰 목록 업데이트
+    }
+
+    private class FetchMovieTitlesTask extends AsyncTask<Void, Void, List<String>> {
+        @Override
+        protected List<String> doInBackground(Void... voids) {
+            List<String> movieTitles = new ArrayList<>();
+
+            Date date = new Date();//현재 날짜와 시간을 가진 객체
+            date.setTime(date.getTime() - (1000 * 60 * 60 * 24)); //현재 날짜의 1일을 뺀 날짜
+
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd"); //SimpleDateFormat 자동으로 편리하게 포맷을 넣을 수 있다.
+            String dateStr = sdf.format(date);
+
+            String address = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.xml"
+                    + "?key=" + apiKey
+                    + "&targetDt=" + dateStr
+                    + "&itemPerPage=10";
+
+            try {
+                URL url = new URL(address);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.connect();
+
+                int responseCode = connection.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    InputStream inputStream = connection.getInputStream();
+                    movieTitles = parseMovieNames(inputStream);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return movieTitles;
+        }
+
+        @Override
+        protected void onPostExecute(List<String> movieTitles) {
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(ReviewActivity.this,
+                    android.R.layout.simple_spinner_item, movieTitles);
+            spinnerMovie.setAdapter(adapter);
+        }
+    }
+
+    private List<String> parseMovieNames(InputStream inputStream) throws XmlPullParserException {
+        List<String> movieNames = new ArrayList<>();
+
+        try {
+            XmlPullParserFactory xmlFactoryObject = XmlPullParserFactory.newInstance();
+            XmlPullParser xmlPullParser = xmlFactoryObject.newPullParser();
+            xmlPullParser.setInput(inputStream, null);
+
+            int eventType = xmlPullParser.getEventType();
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                if (eventType == XmlPullParser.START_TAG && xmlPullParser.getName().equals("movieNm")) {
+                    eventType = xmlPullParser.next();
+                    String movieName = xmlPullParser.getText();
+                    movieNames.add(movieName);
+                }
+                eventType = xmlPullParser.next();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return movieNames;
+    }
+
+    private void saveReview() {
+        String movie = spinnerMovie.getSelectedItem().toString();
+        String name = editTextName.getText().toString();
+        String review = editTextReview.getText().toString();
+
+        ContentValues values = new ContentValues();
+        values.put("movie", movie);
+        values.put("name", name);
+        values.put("review", review);
+
+        long newRowId = database.insert("reviews", null, values);
+        if (newRowId != -1) {
+            Toast.makeText(this, "한 줄 평이 저장되었습니다.", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "한 줄 평 저장에 실패했습니다.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void updateReviewList() {
+        List<String> reviewList = new ArrayList<>();
+        Cursor cursor = database.query("reviews", null, null, null, null, null, null);
+        while (cursor.moveToNext()) {
+            String movie = cursor.getString(cursor.getColumnIndex("movie"));
+            String name = cursor.getString(cursor.getColumnIndex("name"));
+            String review = cursor.getString(cursor.getColumnIndex("review"));
+            reviewList.add(0, movie + " - " + name + ": " + review); // 위로 추가될 수 있도록 인덱스 0에 추가
+        }
+        cursor.close();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, reviewList);
+        ListView listView = findViewById(R.id.listViewReviews);
+        listView.setAdapter(adapter);
+    }
+
+    public class ReviewDatabaseHelper extends SQLiteOpenHelper {
+        private static final String DATABASE_NAME = "reviewDB";
+        private static final int DATABASE_VERSION = 1;
+
+        public ReviewDatabaseHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            String createTableSQL = "CREATE TABLE reviews (_id INTEGER PRIMARY KEY AUTOINCREMENT, movie TEXT, name TEXT, review TEXT)";
+            db.execSQL(createTableSQL);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            // 테이블 업그레이드를 위한 로직 작성 (필요시 구현)
+        }
+    }
+}
